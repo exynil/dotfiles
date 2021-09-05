369abbecd90978493695e1a61d5581f0917c12ba
Maxim Kim <exynil@gmail.com>
2021-09-06 00:15:22 +0600

    dxhd

---
+++ dxhd/.config/dxhd/dxhd.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+## Терминал
+# super + Return
+$TERMINAL
+
+## Перезагрузка dxhd
+# super + Escape
+pkill -USR1 -x dxhd
+
+## Выход и перезагрузка bspwm
+# super + alt + {q,r}
+bspc {quit,wm -r}
+
+## Закрытие окна
+# super + shift + q
+bspc node -c
+
+## alternate between the tiled and monocle layout
+# alt + m
+bspc desktop -l next
+
+##--------------------- state/flags ---------------------##
+
+## set the window state
+# alt + {t,shift + t,s,f}
+bspc node -t {tiled,pseudo_tiled,floating,fullscreen}
+
+## set the node flags
+# alt + ctrl + {m,x,y,z}
+bspc node -g {marked,locked,sticky,private}
+
+##--------------------- focus/swap ---------------------##
+
+## focus the node in the given direction
+# super + {_,shift + }{h,j,k,l}
+bspc node -{f,s} {west,south,north,east}
+
+# super + {_,shift + }{Left,Down,Up,Right}
+bspc node -{f,s} {west,south,north,east}
+
+## focus the node for the given path jump
+# super + {p,b,comma,period}
+bspc node -f @{parent,brother,first,second}
+
+## focus the next/previous window in the current desktop
+# super + {_,shift +} Tab
+bspc node -f {next,prev}.local.!hidden.window
+
+## focus or send to the given desktop
+# super + {_,shift + } {1-9,0}
+bspc {desktop -f,node -d} '{1-9,10}'
+
+## focus or send to the given desktop
+# super + {_,shift + } KP_{1-9,0}
+bspc {desktop -f,node -d} '{1-9,10}'
+
+##--------------------- Preselect ---------------------##
+
+## preselect the direction
+# super + ctrl + {h,j,k,l}
+bspc node -p {west,south,north,east}
+
+# super + ctrl + {Left,Down,Up,Right}
+bspc node -p {west,south,north,east}
+
+## cancel the preselection for the focused node
+# super + ctrl + space
+bspc node -p cancel
+
+## cancel the preselection for the focused desktop
+# super + ctrl + shift + space
+bspc query -N -d | xargs -I id -n 1 bspc node id -p cancel
+
+## --------------------- move/resize ---------------------##
+
+## expand a window by moving one of its side outward
+# super + alt + {h,j,k,l}
+bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}
+
+# super + alt + {Left,Down,Up,Right}
+bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}
+
+## contract a window by moving one of its side inward
+# super + alt + shift + {h,j,k,l}
+bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}
+
+# super + alt + shift + {Left,Down,Up,Right}
+bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}
+
+## move a floating window
+# super + {Left,Down,Up,Right}
+bspc node -v {-20 0,0 20,0 -20,20 0}
+
+##--------------------- Rofi ---------------------##
+
+# super + {d,w,x,m}
+$XDG_CONFIG_HOME/rofi/scripts/{run,window,powermenu,display}
+
+# super + {c,s,q,o}
+$XDG_CONFIG_HOME/rofi/scripts/{clipboard,browser,calculator,conference}
+
+# super + {v,r,e,a}
+$XDG_CONFIG_HOME/rofi/scripts/{colorscheme,record,wallpaper,code}
+
+# super + {z,slash}
+$XDG_CONFIG_HOME/rofi/scripts/{menu,command}
+
+##--------------------- Screenshot ---------------------##
+
+## gui
+# Print
+flameshot gui
+
+## fullscreen
+super + Print
+flameshot full -c
+
+##--------------------- watching youtube video with MPV ---------------------##
+
+# super + y
+sleep 0.5 && xdotool key "ctrl+l" && xdotool key "ctrl+c" && mpv $(xclip -o)
+
+##--------------------- Backlight ---------------------##
+
+# XF86MonBrightness{Up,Down}
+xbacklight {-inc 10,-dec 10}
+
+##--------------------- Audio ---------------------##
+
+# XF86Audio{Play,Stop,Prev,Next}
+playerctl {play-pause,stop,previous,next}
+
+# XF86Audio{RaiseVolume,LowerVolume,Mute}
+pamixer {-i 2,-d 2,-t}
+
+##--------------------- user setup ---------------------##
+
+## Hide polybar
+# super + i
+polybar-toggle
+
+## Termbin
+#super + t
+termbin
+
+## Translate selection
+#super + g
+translate
+
+## htop
+#ctrl + alt + Delete
+$TERMINAL -e htop -H
